a0e1d3a7d7db4617970621d16d5a0300964d5d73
GeralmyUrena <geralmyuri11@gmail.com>
2023-07-20 13:20:17 -0400

    commit tree 5bb93cc6df2a41afde33a9480c402a82071998d4

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validates :content, length: {minimum: 100}
 end
+#1. The title cannot be blank
+# 2. The category is either `"Fiction"` or `"Non-Fiction"`
+# 3. The content is at least 100 characters long
